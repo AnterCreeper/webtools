678b4d6abf0c804b883e03ba2d621f83d909e2b2
Zhihao Wang <wangzhihao9@hotmail.com>
2026-03-20 12:33:58 +0800

    fix listen address issue.

--- install.sh
+++ install.sh
@@ -9,7 +9,9 @@ cd ./qt-web-extractor
 python3 -m build --wheel --no-isolation
 python3 -m installer dist/*.whl
 install -Dm644 qt-web-extractor.conf.example /etc/webtools/qt-web-extractor.conf
+sed -i 's|HOST=127.0.0.1|HOST=0.0.0.0|g' /etc/webtools/qt-web-extractor.conf
 
 cd ../GUILessBingSearch
 install -Dm755 guiless_bing_search.py /usr/local/bin/guiless-bing-search
 install -Dm644 guiless-bing-search.conf.example /etc/webtools/guiless-bing-search.conf
+sed -i 's|HOST=127.0.0.1|HOST=0.0.0.0|g' /etc/webtools/guiless-bing-search.conf
